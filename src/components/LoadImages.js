7d4117c3de083dcdd0f4667ae3bf2d5e60942506
Eric <ericchang00@gmail.com>
2018-10-07 03:36:58 -0700

    remove extraneous components dir

--- src/components/LoadImages.js
+++
@@ -1,8 +0,0 @@
-import charlotte_1 from '../assets/images/charlotte/1.jpg';
-import charlotte_2 from '../assets/images/charlotte/2.jpg';
-
-export default [
-    charlotte_1,
-    charlotte_2,
-];
-
